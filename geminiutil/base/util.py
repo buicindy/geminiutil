1336b3f6596f8df913d7fb13163489519a54c50f
buicindy <cindy.bui@mail.utoronto.ca>
2013-05-04 09:38:04 -0300

    Update util.py
    Added code for the binning parser in Issue #17.

--- geminiutil/base/util.py
+++ geminiutil/base/util.py
@@ -1,2 +1,11 @@
 # Small utilities scripts
 
+def parse_binning_information(binning_information):
+  """ This function parses binning information, given as a string (e.g. "2 2"),
+  to to two ints (2, 2).
+  """
+
+  # Split the string & convert to integers to be returned 
+  x_binning, y_binning = (int(n) for n in binning_information.split())
+  
+  return x_binning, y_binning
